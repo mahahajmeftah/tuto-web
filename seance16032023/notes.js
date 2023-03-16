88d55983a5d6249d93e945d09a6487ee34f18f93
Haj Meftah Maha <77198470+mahahajmeftah@users.noreply.github.com>
2023-03-16 16:22:17 +0100

    Add files via upload

---
+++ seance16032023/notes.js
@@ -0,0 +1,9 @@
+
+//Question1
+//ecrire un script qui permet de trier le tableau selon la moyenne dans l'ordre
+//decroissant
+
+
+//Question2
+//ecrire un script qui permet d'ajouter une nouvelle colonne "resultat"
+//"admis" ou "redouble" selon la moyenne(<10 ou>10)

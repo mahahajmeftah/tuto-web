58b5f4b6d6af2c51ee621f64f1e53d8c651c05d3
Haj Meftah Maha <77198470+mahahajmeftah@users.noreply.github.com>
2023-03-23 16:03:53 +0100

    Add files via upload

---
+++ seance23032023/script.js
@@ -0,0 +1,20 @@
+console.log("ok bb");
+let employees = [
+  ['John', 'Doe', 2500, 'Marketing', 'john.doe@example.com'],
+  ['Maha', 'Hmf', 4000, 'Marketing', 'maha.hmf@example.com'],
+  ['Jane', 'Smith', 1800, 'Sales', 'jane.smith@example.com'],
+  ['Bob', 'Johnson', 3000, 'Finance', 'bob.johnson@example.com'],
+  ['Alice', 'Williams', 2200, 'Human Resources', 'alice.williams@example.com'],
+  ['David', 'Brown', 1900, 'Operations', 'david.brown@example.com']
+];
+
+
+//question1: compter le nombre des salaries qui touchent pus que 2000
+
+
+//question 2: meme question  utiliser .filter
+
+//question 3: ajouter au tableau Html le nom d'employee et son salaire
+
+
+// question 4: creer objet stockant le nbre des employes par departement et fait l'affichage
